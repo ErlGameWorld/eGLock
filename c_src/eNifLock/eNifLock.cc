fda76baa0a2ed607cb57b985b692ca5b811701ca
SisMaker <1713699517@qq.com>
2025-11-13 00:07:30 +0800

    ft: 优化一下内存序 优化原子类型

--- c_src/eNifLock/eNifLock.cc
+++ c_src/eNifLock/eNifLock.cc
@@ -1,65 +1,69 @@
 #include "erl_nif.h"
 #include <atomic>
+#include <type_traits>
 
-using namespace std;
+// 编译期自检：确保ERL_NIF_TERM是可平凡复制的类型
+static_assert(std::is_trivially_copyable<ERL_NIF_TERM>::value, "ERL_NIF_TERM must be trivially copyable");
 
 const int LockSize = 2097152;
-atomic<uint64_t> LockSlot[LockSize];
+std::atomic<ERL_NIF_TERM> LockSlot[LockSize];
 
 ERL_NIF_TERM atomTrue;
 ERL_NIF_TERM atomFalse;
 ERL_NIF_TERM atomUndefined;
 
-int nifLoad(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM load_info){
+int nifLoad(ErlNifEnv *env, void **priv_data, ERL_NIF_TERM load_info) {
     atomTrue = enif_make_atom(env, "true");
     atomFalse = enif_make_atom(env, "false");
     atomUndefined = enif_make_atom(env, "undefined");
     return 0;
 }
 
-inline bool lockOne(ErlNifEnv *env, ErlNifPid *ThePid, int KeyIx, uint64_t Val){
-    uint64_t Expected = 0;
-    if (LockSlot[KeyIx].compare_exchange_strong(Expected, Val)){
+inline bool lockOne(ErlNifEnv *env, ErlNifPid *ThePid, int KeyIx, ERL_NIF_TERM Val) {
+    ERL_NIF_TERM Expected = 0;
+    if (LockSlot[KeyIx].compare_exchange_strong(Expected, Val, std::memory_order_acquire, std::memory_order_relaxed)) {
         return true;
-    }else{
-        ThePid->pid = (ERL_NIF_TERM)Expected;
-        if (enif_is_process_alive(env, ThePid)){
+    } else {
+        ThePid->pid = Expected;
+        if (enif_is_process_alive(env, ThePid)) {
             return false;
-        }else{
-            return LockSlot[KeyIx].compare_exchange_strong(Expected, Val);
+        } else {
+            return LockSlot[KeyIx].compare_exchange_strong(Expected, Val, std::memory_order_acquire, std::memory_order_relaxed);
         }
     }
 }
 
-ERL_NIF_TERM tryLock(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
+ERL_NIF_TERM tryLock(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     int KeyIx;
     enif_get_int(env, argv[0], &KeyIx);
     ErlNifPid ThePid;
     enif_self(env, &ThePid);
-    uint64_t Val = (uint64_t)(ThePid.pid);
+    ERL_NIF_TERM Val = ThePid.pid;
 
     return lockOne(env, &ThePid, KeyIx, Val) ? atomTrue : atomFalse;
 }
 
-ERL_NIF_TERM tryLocks(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
+ERL_NIF_TERM tryLocks(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     ERL_NIF_TERM allList = argv[0];
     ERL_NIF_TERM head;
     ErlNifPid ThePid;
     enif_self(env, &ThePid);
-    uint64_t Val = (uint64_t)(ThePid.pid);
+    const ERL_NIF_TERM Val = ThePid.pid;
     int KeyIx;
     int cnt = -1;
 
-    while (enif_get_list_cell(env, allList, &head, &allList)){
+    while (enif_get_list_cell(env, allList, &head, &allList)) {
         enif_get_int(env, head, &KeyIx);
-        if(lockOne(env, &ThePid, KeyIx, Val)){
+        if (lockOne(env, &ThePid, KeyIx, Val)) {
             cnt++;
-        }else{
+        } else {
             allList = argv[0];
-            for(int i = 0; i <= cnt; i++){
-               enif_get_list_cell(env, allList, &head, &allList);
-               enif_get_int(env, head, &KeyIx);
-               LockSlot[KeyIx].store(0);
+            ERL_NIF_TERM Expected;
+            for (int i = 0; i <= cnt; i++) {
+                enif_get_list_cell(env, allList, &head, &allList);
+                enif_get_int(env, head, &KeyIx);
+                Expected = Val;
+                LockSlot[KeyIx].compare_exchange_strong(Expected, 0, std::memory_order_release, std::memory_order_relaxed);
             }
             return atomFalse;
         }
@@ -67,46 +71,45 @@ ERL_NIF_TERM tryLocks(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
     return atomTrue;
 }
 
-ERL_NIF_TERM releaseLock(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
+ERL_NIF_TERM releaseLock(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     int KeyIx;
     enif_get_int(env, argv[0], &KeyIx);
     ErlNifPid ThePid;
     enif_self(env, &ThePid);
-    uint64_t Expected = (uint64_t)(ThePid.pid);
-
-    return LockSlot[KeyIx].compare_exchange_strong(Expected, 0) ? atomTrue : atomFalse;
+    ERL_NIF_TERM Expected = ThePid.pid;
+    return LockSlot[KeyIx].compare_exchange_strong(Expected, 0, std::memory_order_release, std::memory_order_relaxed) ? atomTrue : atomFalse;
 }
 
-ERL_NIF_TERM releaseLocks(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
+ERL_NIF_TERM releaseLocks(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     ERL_NIF_TERM allList = argv[0];
     ERL_NIF_TERM head;
     ErlNifPid ThePid;
     enif_self(env, &ThePid);
-    uint64_t Expected = (uint64_t)(ThePid.pid);
-    uint64_t RExpected;
+    const ERL_NIF_TERM Expected = ThePid.pid;
+    ERL_NIF_TERM RExpected;
     int KeyIx;
     int isAllOk = 1;
 
-    while (enif_get_list_cell(env, allList, &head, &allList)){
+    while (enif_get_list_cell(env, allList, &head, &allList)) {
         enif_get_int(env, head, &KeyIx);
         RExpected = Expected;
-        if (!LockSlot[KeyIx].compare_exchange_strong(RExpected, 0)){
+        if (!LockSlot[KeyIx].compare_exchange_strong(RExpected, 0, std::memory_order_release, std::memory_order_relaxed)) {
             isAllOk = 0;
         }
     }
     return isAllOk > 0 ? atomTrue : atomFalse;
 }
 
-ERL_NIF_TERM getLockPid(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]){
+ERL_NIF_TERM getLockPid(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     int KeyIx;
     enif_get_int(env, argv[0], &KeyIx);
     ErlNifPid ThePid;
 
-    uint64_t Var = LockSlot[KeyIx].load();
-    if (Var > 0){
-        ThePid.pid = (ERL_NIF_TERM)Var;
+    ERL_NIF_TERM Var = LockSlot[KeyIx].load(std::memory_order_acquire);
+    if (Var != 0) {
+        ThePid.pid = Var;
         return enif_make_pid(env, &ThePid);
-    }else{
+    } else {
         return atomUndefined;
     }
 }
